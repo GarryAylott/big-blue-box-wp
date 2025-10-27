253d753ccee7f094416750f0bcd9a8ddaa800202
Garry Aylott <gazaylott@gmail.com>
2025-10-27 16:01:22 +0000

    About page finished and other styling updates.

--- page-templates/page-about.php
+++ page-templates/page-about.php
@@ -89,10 +89,122 @@ get_header();
             </div>
         </section>
 
-        <section class="about-page-writers">
+        <section class="about-page-writers flow-large">
              <div class="text-block">
                 <h2>Writing Team</h2>
                 <p>The blog is driven by writers who know the show inside out. Their articles range across Big Finish reviews, convention round-ups, editorials and more. Also active in our Discord, they're always on hand to chat anything Doctor Who.</p>
+            </div>
+
+            <?php
+            $team_writers = [
+                [
+                    'user_id'   => 4,
+                    'name'      => 'Maria Kalatichou',
+                    'role'      => 'Writer',
+                    'image'     => 'images/authors/about-writers-panel-maria.webp',
+                    'image_alt' => 'Maria Kalatichou - Staff writer',
+                ],
+                [
+                    'user_id'   => 8,
+                    'name'      => 'Matt Steele',
+                    'role'      => 'Writer & Merch Reporter',
+                    'image'     => 'images/authors/about-writers-panel-matt.webp',
+                    'image_alt' => 'Matt Steele - Staff writer and Merch Reporter',
+                ],
+                [
+                    'user_id'   => 3,
+                    'name'      => 'Jordan Shortman',
+                    'role'      => 'Writer',
+                    'image'     => 'images/authors/about-writers-panel-jordan.webp',
+                    'image_alt' => 'Jordan Shortman - Staff writer',
+                ],
+                [
+                    'user_id'   => 7,
+                    'name'      => 'Harry Walker',
+                    'role'      => 'Writer',
+                    'image'     => 'images/authors/about-writers-panel-harry.webp',
+                    'image_alt' => 'Harry Walker - Staff writer',
+                ],
+            ];
+
+            $social_icon_map = [
+                'twitter' => [
+                    'label' => esc_html__('Twitter', 'bigbluebox'),
+                    'icon'  => 'social-icon-x.svg',
+                    'class' => 'author-social-x',
+                ],
+                'instagram' => [
+                    'label' => esc_html__('Instagram', 'bigbluebox'),
+                    'icon'  => 'social-icon-insta.svg',
+                    'class' => 'author-social-insta',
+                ],
+                'facebook' => [
+                    'label' => esc_html__('Facebook', 'bigbluebox'),
+                    'icon'  => 'social-icon-fb.svg',
+                    'class' => 'author-social-fb',
+                ],
+                'tiktok' => [
+                    'label' => esc_html__('TikTok', 'bigbluebox'),
+                    'icon'  => 'social-icon-tiktok.svg',
+                    'class' => 'author-social-tiktok',
+                ],
+            ];
+            ?>
+            <div class="team-writers">
+                <?php foreach ($team_writers as $writer) : ?>
+                    <?php
+                    $user_key = 'user_' . $writer['user_id'];
+                    $social_links = [];
+
+                    foreach ($social_icon_map as $field_key => $icon_meta) {
+                        $link_url = get_field($field_key, $user_key);
+                        if (!empty($link_url)) {
+                            $social_links[$field_key] = $link_url;
+                        }
+                    }
+
+                    $first_name = get_user_meta($writer['user_id'], 'first_name', true);
+                    if (empty($first_name)) {
+                        $first_name = $writer['name'];
+                    }
+                    $first_name = sanitize_text_field($first_name);
+                    ?>
+                    <div class="team-writers__panel">
+                        <div class="team-writers__content">
+                            <h5><?php echo esc_html($writer['name']); ?></h5>
+                            <p class="small"><?php echo esc_html__($writer['role'], 'bigbluebox'); ?></p>
+                            <?php if (!empty($social_links)) : ?>
+                                <div class="author-hero__socials">
+                                    <?php foreach ($social_links as $network => $url) : ?>
+                                        <?php $icon_meta = $social_icon_map[$network]; ?>
+                                        <a href="<?php echo esc_url($url); ?>" class="has-external-icon" target="_blank" rel="noopener" aria-label="<?php echo esc_attr($icon_meta['label']); ?>">
+                                            <img class="<?php echo esc_attr($icon_meta['class']); ?> img-hover" src="<?php echo esc_url(get_template_directory_uri() . '/images/icons/' . $icon_meta['icon']); ?>" alt="<?php echo esc_attr($icon_meta['label']); ?>" width="24" height="24">
+                                        </a>
+                                    <?php endforeach; ?>
+                                </div>
+                            <?php endif; ?>
+                            <a class="team-writers__link" href="<?php echo esc_url(get_author_posts_url($writer['user_id'])); ?>">
+                                <button class="button"><?php printf(esc_html__('More about %s', 'bigbluebox'), esc_html($first_name)); ?></button>
+                            </a>
+                        </div>
+                        <img class="team-writers__image" src="<?php echo esc_url(get_theme_file_uri($writer['image'])); ?>" alt="<?php echo esc_attr($writer['image_alt']); ?>" />
+                    </div>
+                <?php endforeach; ?>
+
+                <div class="team-writers__panel">
+                    <div class="team-writers__content">
+                        <h5>
+                            Maybe You?
+                        </h5>
+                        <p class="small" id="panel5-title">
+                            Writers with a love for Doctor Who — we want you! We’re always on the lookout for writers who can craft great articles. If that's you and want to be part of a welcoming, creative crew, drop us a message.
+                        </p>
+                        <a class="button" href="mailto:hello@bigblueboxpodcast.co.uk?subject=I%27m%20interested%20in%20writing%20for%20The%20Big%20Blue%20Box!">
+                            Get in Touch
+                        </a>
+                    </div>
+                    <img class="team-writers__image" src="<?php echo esc_url(get_theme_file_uri('images/authors/about-writers-panel-maybeyou.webp')); ?>" alt="Future Big Blue Box writer"/>
+                </div>
             </div>
         </section>
 

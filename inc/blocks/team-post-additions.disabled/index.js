43fcc6e70826cb7d2a1ff7787cec8e084181330b
Garry Aylott <gazaylott@gmail.com>
2025-12-05 14:30:15 +0000

    Remove previous blocks code to start fresh

--- inc/blocks/team-post-additions.disabled/index.js
+++
@@ -1,8 +0,0 @@
-import Edit from "./edit";
-import metadata from "./block.json";
-import { registerBlockType } from "@wordpress/blocks";
-
-registerBlockType(metadata.name, {
-    edit: Edit,
-    save: () => null, // Rendered in PHP
-});
